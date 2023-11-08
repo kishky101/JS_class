a871b644ab070c4e3fc3d74feddff113d77f4f8a
Marzuq Adebayo <kishky.101@gmail.com>
2023-11-08 21:29:21 +0100

    formatted code

--- conditonals.js
+++ conditonals.js
@@ -1,33 +1,33 @@
 //if statement
-    /*
+/*
         SYNTAX
         if (condition(s) === true) {
           run  block of codes 
             if condition evaluates to true
         }
     */
-        let a = 5;
-        // a = 5 ---> assignment operator
+let a = 5;
+// a = 5 ---> assignment operator
 
-        // a == 5 ---> weak equality
-         console.log( a == '5' )
-         console.log( a == 5 )
+// a == 5 ---> weak equality
+console.log(a == "5");
+console.log(a == 5);
 
-        //type coersion
-        console.log(5 == '5')
-        console.log(20 + 'you')
-        console.log(20 - 'you')
+//type coersion
+console.log(5 == "5");
+console.log(20 + "you");
+console.log(20 - "you");
 
-        // a === 5 ---> strict equality
-        console.log( a === '5' )
-        console.log( a === 5 )
+// a === 5 ---> strict equality
+console.log(a === "5");
+console.log(a === 5);
 
-        if (a === 5) {
-            console.log(a)
-        }
+if (a === 5) {
+  console.log(a);
+}
 
 //if-else
-    /*
+/*
         SYNTAX
         if (condition(s)) {
           run  block of codes 
@@ -37,16 +37,16 @@
             if condition evaluates to false
         }
     */
-        a = 19
+a = 19;
 
-        if (a === 5) {
-            console.log(5)
-        }else {
-            console.log(a)
-        }
-        
+if (a === 5) {
+  console.log(5);
+} else {
+  console.log(a);
+}
+
 //if-and else-if
-    /*
+/*
         SYNTAX
         if (condition(s)) {
           run  block of codes 
@@ -60,37 +60,30 @@
         }
     */
 
-        let newYear = 'faster';
-        if (newYear === 'slow') {
-            console.log('what a year')
-        }else if (newYear === 'faster' && newYear === 'slower' || a === 19) {
-            console.log('kishky got a job')
-        }else {
-            console.log(newYear)
-        }
+let newYear = "faster";
+if (newYear === "slow") {
+  console.log("what a year");
+} else if ((newYear === "faster" && newYear === "slower") || a === 19) {
+  console.log("kishky got a job");
+} else {
+  console.log(newYear);
+}
 
 //tenary operator
 //switch statement
 
-
-
-
-
 //Logical Operators
-        // Logical OR operator    ---> ||
-        // Logical AND operator   ---> &&
-
-
+// Logical OR operator    ---> ||
+// Logical AND operator   ---> &&
 
 // TRUTHY                      AND                        FALSY VALUES
-    // numbers greater than 0 or                            // 0
-    // less than 0                                          // false                              
-    // string                                               // undefined
-    // true                                                 // null
-    // []                                                   // ''
-    // {}
+// numbers greater than 0 or                            // 0
+// less than 0                                          // false
+// string                                               // undefined
+// true                                                 // null
+// []                                                   // ''
+// {}
 
-
-    if (0) {
-        console.log(a)
-    }
+if (0) {
+  console.log(a);
+}
--- function.js
+++ function.js
@@ -2,89 +2,100 @@
 
 //definations with {let , const, var}
 
-'[0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j]'
-
+"[0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j]";
 
 /**
- * 255 / 16 
+ * 255 / 16
  * 15  /16     r 15 f
  * 0           r 15 f
  * rgb(255, 255, 255)
  * #ffffff
  * ff
  * -----------------------
- * 
- * 255 / 20    
+ *
+ * 255 / 20
  * 12  /20     r 15 f
  * 0           r 12 c
  */
 
 function base20(number) {
-    const base20Array = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J'];
-    let numTObase20 = number;
-    let finalArray = [];
-    let remainder ; 
+  const base20Array = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+  ];
+  let numTObase20 = number;
+  let finalArray = [];
+  let remainder;
 
-    while (numTObase20 > 20) {
-        remainder =  numTObase20 % 20
-        numTObase20 = parseInt(numTObase20 / 20)
-        finalArray.unshift(base20Array[remainder])
-    }
+  while (numTObase20 > 20) {
+    remainder = numTObase20 % 20;
+    numTObase20 = parseInt(numTObase20 / 20);
+    finalArray.unshift(base20Array[remainder]);
+  }
 
-    finalArray.unshift(base20Array[numTObase20])
-    return finalArray.join('')
+  finalArray.unshift(base20Array[numTObase20]);
+  return finalArray.join("");
 
-    /**
-     * CONTINUE TALKING THIS IS THE ANSWER TO YOUR BASE 20 PROBLEM
-     * IT'S 11:30PM ALREADY AND YOU'VE WASTED ALL OUR TIME TODAY
-     * GOOD NIGHT ZZZ
-     */
+  /**
+   * CONTINUE TALKING THIS IS THE ANSWER TO YOUR BASE 20 PROBLEM
+   * IT'S 11:30PM ALREADY AND YOU'VE WASTED ALL OUR TIME TODAY
+   * GOOD NIGHT ZZZ
+   */
 }
-console.log(base20(2557))
-
+console.log(base20(2557));
 
 /**
  * L = [1, 3, 5, 2, 7, 11, 15] , n = 8 ///  7
- * filterL = [1, 3, 5, 2, 7] 
+ * filterL = [1, 3, 5, 2, 7]
  * sortFilerL = sort(filterL) //[1, 2, 3, 5, 7]
  * return num at sortFilterL[sortFilterL.length-1]; // 7
  */
 
-function largestNumLessThanN() {
-
-}
-
+function largestNumLessThanN() {}
 
 function abuStandUp() {
-    //parent scope
-    let abu = 'stand up';
+  //parent scope
+  let abu = "stand up";
 
+  function abuStandup2() {
+    //child scope
+    let food;
+    food = "meat";
+    abu = "sit down";
+    console.log(abu);
+  }
 
-    function abuStandup2() {
-
-          //child scope
-        let food;
-        food = 'meat'
-        abu = 'sit down';
-        console.log(abu)
-    }
-
-    console.log(food)
-
+  console.log(food);
 }
 
-
-
-abuStandUp()
-console.log(abu)
+abuStandUp();
+console.log(abu);
 //function expression
 
-abuSitDown = function() {
-    console.log(abu)
+abuSitDown = function () {
+  console.log(abu);
 };
 
-abuSitDown()
+abuSitDown();
 
-//Anonymous function 
+//Anonymous function
 
-//i  am not talking , i'm just typing 
+//i  am not talking , i'm just typing
